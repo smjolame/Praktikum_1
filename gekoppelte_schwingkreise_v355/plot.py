81cfacccecdadbe0e150c246dd9c4c2f0ebb1ce2
Johannes Lamers <smjolame@tu-dortmund.de>
2019-12-09 18:15:18 +0100

    Stuff

--- gekoppelte_schwingkreise_v355/plot.py
+++ gekoppelte_schwingkreise_v355/plot.py
@@ -17,13 +17,14 @@ def n(C_k):
 
 C_k = np.linspace(2*10**(-9), 12*10**(-9), 1000)
 
-plt.plot(C_k,n(C_k))
+plt.plot(C_k,n(C_k), label='theoretische Kurve')
 
 f1,Ck,ne=np.genfromtxt('data/messwerte_a.txt',delimiter=',',unpack=True)
-plt.plot(Ck*10**(-9),ne/2,'rx')
+plt.plot(Ck*10**(-9),ne/2,'rx', label='gemessene Werte')
 plt.xlabel(r'$C_k \:/\:\si{\farad}$')
 plt.ylabel(r'$n_t$')
 plt.grid()
+plt.legend()
 plt.savefig('build/amplituden.pdf')
 plt.clf()
 
@@ -39,19 +40,23 @@ def I(w,R,C_k,U):
 
 
 Ck, v, U =np.genfromtxt('data/messwerte_b_pi.txt',delimiter=',',unpack=True)
-plt.plot(C_k,1/(2*np.pi*np.sqrt(L*((1/C+2/C_k)**(-1)+C_sp))))
-plt.plot(Ck*10**(-9),v*10**3,'rx')
+plt.plot(C_k,1/(2*np.pi*np.sqrt(L*((1/C+2/C_k)**(-1)+C_sp))), label='theoretische Kurve')
+plt.plot(Ck*10**(-9),v*10**3,'rx',label='gemessene Werte')
 plt.ylabel(r'$\nu^-\:/\: \si{\hertz}$')
 plt.xlabel(r'$C_k \:/\:\si{\farad}$')
 plt.grid()
+plt.legend()
 plt.savefig('build/frequenzen.pdf')
 plt.clf()
 
 f=np.linspace(5,50000,10000)
-plt.plot(f,I(2*np.pi*f,73,Ck[0]*10**(-9),U[0]))
+plt.plot(f,I(2*np.pi*f,73,Ck[0]*10**(-9),U[0]),label='theoretische Kurve')
 plt.grid()
-plt.plot(37900,0.089,'rx')
+plt.plot(37900,0.089,'rx', label='gemessene Werte')
 plt.plot(35600,0.098,'rx')
+plt.xlabel(r'$\nu^-\:/\: \si{\hertz}$')
+plt.ylabel(r'$I \:/\: \si{\ampere}$')
+plt.legend()
 plt.savefig('build/strom.pdf')
 plt.clf()
 
