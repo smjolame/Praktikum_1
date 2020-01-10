8fbac0afcb848b842fa93f7f723aae9e3243e4fb
Johannes Lamers <smjolame@tu-dortmund.de>
2020-01-10 11:17:31 +0100

    Stuff

--- Traegheitsmoment_V101/plot.py
+++ Traegheitsmoment_V101/plot.py
@@ -1,21 +1,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from uncertainties import ufloat
+from scipy import stats
+from scipy.optimize import curve_fit
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+def D(F,r,phi):
+    return F*r/phi
+def reg(x,a,b):
+    return a*x+b
+def I_symz(m,R):
+    return m/2*R**2
+def I_asymz(m,R,h):
+    return m*((R**2)/4+(h**2)/12)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+
+phi, F = np.genfromtxt('data/feder.txt', delimiter=',',unpack=True)
+
+D=D(F,0.1,phi*np.pi/180)
+D_err=stats.sem(D)
+D=np.mean(D)
+data = [D,D_err]
+D_u=ufloat(D,D_err)
+np.savetxt('D.txt',data,fmt="%1.6f")
+
+print(D_u)
+print(D_err)
+
+def I(T):
+    return D*T**2/(4*np.pi**2)
+
+r , T = np.genfromtxt('data/eigentr.txt',delimiter=',',unpack=True)
+r=r*10**(-2)
+
+params, cov = curve_fit(reg,r**2,T**2)
+I_D_err=np.sqrt(cov[1,1])
+I_D=ufloat(params[1],I_D_err)/(4*np.pi**2)
+print(I_D)    
+
+x=np.linspace(r[0],r[np.size(r)-1])
+
+plt.plot(r**2,T**2,'rx',label='Quadtrat der Messwerte')
+plt.plot(x**2,reg(x**2,params[0],params[1]),'b-',label='Ausgleichsgerade')
+plt.grid()
+plt.xlabel(r'r')  #!!!!
+plt.ylabel(r'T')  #!!!!
+plt.legend()
+
+
+
