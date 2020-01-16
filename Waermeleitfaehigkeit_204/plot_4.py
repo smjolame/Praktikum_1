9ec8e077b2857fbfbc1a4b1e149d640ba378bf20
sebastian fischer <sebastian5.fischer@tu-dortmund.de>
2020-01-16 16:13:03 +0100

    weitere plots, die nicht funktionieren

--- Waermeleitfaehigkeit_204/plot_4.py
+++ Waermeleitfaehigkeit_204/plot_4.py
@@ -4,18 +4,13 @@ from uncertainties import ufloat
 from scipy import stats
 from scipy.optimize import curve_fit
 
-T1,T2,T3,T4,T5,T6,T7,T8,t = np.genfromtxt('data/GLXportRun1.txt', unpack=True, delimiter='  ')
+T1,T2,T3,T4,T5,T6,T7,T8,t = np.genfromtxt('data/GLXportRun3.txt', unpack=True, delimiter='  ')
 
-plt.plot(t,T5, 'rx', label='T5')
-plt.plot(t,T8, 'bx', label='T8')
+plt.plot(t,T7, 'rx', label='T7_2')
+plt.plot(t,T8, 'bx', label='T8_2')
 
 plt.grid()
 #plt.xlabel(r'$t\:/\:\si{\s}$')
 #plt.ylabel(r'$T\:/\:\si{\celsius}$')
 plt.legend()
-plt.savefig('data/T5T8')
-
-
-
-
-
+plt.savefig('data/T7T8')
