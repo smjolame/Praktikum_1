e79bfd022a6e5a336619f09767324abe61a004ee
Johannes Lamers <smjolame@tu-dortmund.de>
2020-05-24 18:17:46 +0200

    neues Template und so

---
+++ template/plot.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+import json
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from uncertainties.unumpy import uarray
+from uncertainties import unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms,std_devs as stds)
+
+
+##Curvefit
+#def BeispielFunktion(x,a,b):
+#    return a*x+b 
+#params, cov = curve_fit(BeispielFunktion, x-Werte, y-Werte,sigma=fehler_der_y_werte,p0=[schätzwert_1,#schätzwert_2])
+#a = ufloat(params[0],np.absolute(cov[0][0])**0.5)
+#b = ufloat(params[1],np.absolute(cov[1][1])**0.5)
+#
+#
+##Json
+#Ergebnisse = json.load(open('data/Ergebnisse.json','r')
+#if not 'Name' in Ergebnisse:
+#    Ergebnisse['Name'] = {}
+#Ergebnisse['Name']['Name des Wertes']=Wert
+#
+#json.dump(Ergebnisse,open('data/Ergebnisse.json','w'),indent=4)
