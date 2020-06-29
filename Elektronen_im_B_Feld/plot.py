8db5094bc5d00a0ce6e436c2f42addb4daf3b938
Johannes Lamers <smjolame@tu-dortmund.de>
2020-06-29 12:51:12 +0200

    Sachen gemacht

--- Elektronen_im_B_Feld/plot.py
+++ Elektronen_im_B_Feld/plot.py
@@ -17,13 +17,14 @@ def spez(a,U):
     return a**2*8*U
 
 k_l = 0.05/8 #Kästchenlänge (m)
+
 L = 0.175 #Wirkungsbereich des B Feldes (m)
 mu_0 = 4*np.pi*10**(-7) #magn.Konst.
 N = 20 #Anzahl Windungen der Spulen
 R = 0.282 #Radius der Spulen (m)
 U_1 = 250 #Spannug erste Versuchsdurchführung (V)
 U_2 = 380 #Spannug zweite Versuchsdurchführung (V)
-I_hor = 7 #Strom zum kompensieren des Erdmagnetfeldes (A)
+I_hor = 0.7 #Strom zum kompensieren des Erdmagnetfeldes (A)
 phi = np.radians(58) #Inklinationswinkel (rad)
 #Magnetfeld einer Helmholtzspule
 def B(I):
@@ -33,6 +34,8 @@ I_1, K_1 = np.genfromtxt('data/250V.txt',delimiter=',',unpack=True)
 I_2, K_2 = np.genfromtxt('data/380V.txt',delimiter=',',unpack=True)
 D_1 = K_1*k_l #Ablenkung der Elektronen
 D_2 = K_2*k_l
+
+
  
 V_1 = D_1/(D_1**2+L**2) #Verhältnis zum Bestimmen der Steigung
 V_2 = D_2/(D_2**2+L**2)
@@ -50,22 +53,31 @@ b_2 = ufloat(pa_2[1],np.absolute(cov_2[1][1])**0.5)
 
 spez_1=spez(a_1,U_1)
 spez_2=spez(a_2,U_2)
-print(spez_1)
-print(spez_2)
+
+spez=np.mean([spez_1,spez_2])
+
+spez_l=17.5882001076*10**10 #Literaturwer spez Ladung
+abw_spez=abw(spez_l,spez)
+print(abw_spez)
 #Plots
-plt.plot(B(I_1),V_1,'kx',label='Messwerte bei U=250V')
-plt.plot(B(I_2),V_2,'bx',label='Messwerte bei U=380V')
-plt.plot(B(I_1),gerade(B(I_1),noms(a_1),noms(b_1)),label='Ausgleichsgerade 250V')
-plt.plot(B(I_2),gerade(B(I_2),noms(a_2),noms(b_2)),label='Ausgleichsgerade 380V')
+plt.plot(B(I_1)*10**3,V_1,'kx',label='Messwerte bei U=250V')
+plt.plot(B(I_2)*10**3,V_2,'bx',label='Messwerte bei U=380V')
+plt.plot(B(I_1)*10**3,gerade(B(I_1),noms(a_1),noms(b_1)),label='Ausgleichsgerade 250V')
+plt.plot(B(I_2)*10**3,gerade(B(I_2),noms(a_2),noms(b_2)),label='Ausgleichsgerade 380V')
+plt.xlabel(r'$B \:/\: \si{\milli\tesla}$')
+plt.ylabel(r'$\frac{D}{L^2+D^2} \:/\: \si[per-mode=reciprocal]{\per\m}$')
 plt.grid()
 plt.legend()
-plt.show()
+plt.savefig('build/Kurve.pdf')
 
 ##Erdmagnetfeld
 
 B_hor=B(I_hor)
+
 B_tot=B_hor/np.cos(phi) #Totalintensität Erdmagnetfeld
-print(B_tot)
+B_l=4.91941*10**(-5)
+abw_B=abw(B_l,B_tot)
+print(abw_B)
 
 ##Curvefit
 #def BeispielFunktion(x,a,b):
