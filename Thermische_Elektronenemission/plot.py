b1f06724ac5d43b69fdf9c2cd2ecc536fbc4156c
Johannes Lamers <smjolame@tu-dortmund.de>
2020-07-12 16:22:18 +0200

    V402 theorien und durchführung fertig, V504 auswertung angefangen

--- Thermische_Elektronenemission/plot.py
+++ Thermische_Elektronenemission/plot.py
@@ -10,6 +10,34 @@ from uncertainties.unumpy import (nominal_values as noms,std_devs as stds)
 
 def abw(exact,approx):
     return (exact-approx)*100/exact  #Abweichnung
+def approx(I_s,a,b,U):
+    return I_s-a*np.e**(-b*U)
+U_max, I_max = np.genfromtxt('data/max.txt',delimiter=',',unpack=True)
+U_nmax, I_nmax = np.genfromtxt('data/nmax.txt',delimiter=',',unpack=True)
+
+epsilon0=8.85418782
+e0=1.6021892*10**(-19)
+m0=9.109534*10**(-34)
+
+def j(V,x,a)=4/9*epsilon0*np.sqrt(2e0/m0)*V**x/a**2
+
+
+pa_nmax, cov_nmax = curve_fit(approx,U_nmax,I_nmax)
+print(pa_nmax[0])
+
+plt.plot(U_max,I_max,'kx')
+#plt.plot(U_nmax,I_nmax,'kx')
+#plt.plot(U_nmax,approx(*pa_nmax,U_nmax))
+plt.show()
+
+
+
+
+
+
+
+
+#Plots###############
 
 
 ##Curvefit
